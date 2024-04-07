24a5fac09b12621488fccc9a9f1b38221683e5d8
Zach <zach.Brewer.1995@gmail.com>
2024-04-07 10:38:14 -0400

    coded about page

---
+++ src/pages/About.jsx
@@ -0,0 +1,20 @@
+import aboutme from "../docs/";
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <div className="about">
+        <img src={} alt="Zach Brewer" />
+        <div className="about-content">
+          <h2>Zach Brewer</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ex inventore quod perferendis eum aliquid beatae laudantium ullam consequuntur aliquam deserunt enim, iusto alias soluta distinctio illum nobis, neque voluptate?{" "}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
