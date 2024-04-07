fc42c391936378dd17188a69e89adfc96f1636c3
Zach <zach.Brewer.1995@gmail.com>
2024-04-07 10:48:22 -0400

    coded Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer>
+      <a href='www.linkedin.com/in/zachary-brewer-9b3634204' target='_blank' rel='noreferrer'>LinkedIn</a>
+      <a href='https://github.com/ZacharyDOTpy' target='_blank' rel='noreferrer'>GitHub</a>
+    </footer>
+  );
+}
+
+export default Footer;
