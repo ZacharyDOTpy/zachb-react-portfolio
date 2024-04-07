05ec3837ebdc934ddccc0895c7e67f8779ed25b7
Zach <zach.Brewer.1995@gmail.com>
2024-04-07 10:39:33 -0400

    coded contact me page

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,24 @@
+function Contact() {
+  return (
+    <div>
+      <h2>Contact Me</h2>
+      <form>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" name="name" required />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea id="message" name="message" required />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default Contact;
