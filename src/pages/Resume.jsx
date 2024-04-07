c7d2e0784b4fd4c0c6fcb68d9b85382a7f8ac086
Zach <zach.Brewer.1995@gmail.com>
2024-04-07 10:42:21 -0400

    coded resume page

---
+++ src/pages/Resume.jsx
@@ -0,0 +1,18 @@
+import resumeImage from '../docs/';
+import resumePDF from '../docs/';
+
+function Resume() {
+  return (
+    <div>
+      <h2>Resume</h2>
+      <div className='resume'>
+            <a href={resumePDF} target="_blank" rel="noopener noreferrer">
+              <img src={resumeImage} alt= 'Resume' />
+            </a>
+            <p>Click the image to download PDF</p>
+          </div>
+    </div>
+  );
+}
+
+export default Resume;
